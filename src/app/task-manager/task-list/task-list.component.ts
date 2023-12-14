fc3bec1b6815fe4a1c280696e3ae3303c9822af8
Ilie Ciotir <ilie.ciotir@sap.com>
2023-12-14 14:57:12 +0200

    url navigation

--- src/app/task-manager/task-list/task-list.component.ts
+++ src/app/task-manager/task-list/task-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task, TaskService } from '../task-service/task.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Subject, combineLatest, merge, of } from 'rxjs';
 import { debounceTime, filter, map, tap } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-list',
@@ -12,18 +13,23 @@ import { debounceTime, filter, map, tap } from 'rxjs/operators';
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
+export class TaskListComponent implements AfterViewInit {
   public tasks: Task[] = [];
   public searchInput$ = new Subject<string>();
 
-  constructor(private tasksService: TaskService) {
+  @ViewChild('searchInput')
+  public searchInputField?: ElementRef<HTMLInputElement>;
+
+  constructor(private tasksService: TaskService, private router: Router, private activatedRoute: ActivatedRoute) {
+    const urlSearch = this.activatedRoute.snapshot.queryParams['search'];
+
     this.searchInput$.subscribe(term => console.log(term));
 
     const searchTerm$ = this.searchInput$.pipe(
       debounceTime(500),
       filter(term => term.length >= 2 || term.length === 0)
     );
-    const defaultSearch$ = merge(searchTerm$, of(''))
+    const defaultSearch$ = merge(searchTerm$, of(urlSearch))
 
     combineLatest([this.tasksService.tasks$, defaultSearch$])
       .pipe(
@@ -32,6 +38,19 @@ export class TaskListComponent {
         tap(event => console.log(`dupa`, event))
       )
       .subscribe(tasks => this.tasks = tasks);
+
+    searchTerm$.subscribe(term => {
+      this.router.navigate([], {
+        queryParams: { search: term }
+      })
+    })
+  }
+
+  ngAfterViewInit(): void {
+    console.log(`searchInput`, this.searchInputField)
+    if (this.searchInputField) {
+      this.searchInputField.nativeElement.value = this.activatedRoute.snapshot.queryParams['search'];;
+    }
   }
 
   ngOnInit() {
