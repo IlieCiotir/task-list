081dc7f176309d8207c7bcb2b0c4b9a73faced9d
Ilie Ciotir <ilie.ciotir@sap.com>
2023-12-15 14:01:48 +0200

    reactive editor

--- src/app/task-manager/task-editor/task-editor.component.ts
+++ src/app/task-manager/task-editor/task-editor.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Task, TaskService } from '../task-service/task.service';
 import { Router } from '@angular/router';
 import { catchError, of } from 'rxjs';
@@ -8,24 +8,48 @@ import { catchError, of } from 'rxjs';
 @Component({
   selector: 'app-task-editor',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './task-editor.component.html',
   styleUrl: './task-editor.component.scss'
 })
-export class TaskEditorComponent {
+export class TaskEditorComponent implements OnInit {
 
-  constructor(private taskService: TaskService, private router: Router) {
+  public taskForm = this.fb.group({
+    id: [''],
+    title: ['', Validators.required],
+    description: ['', [Validators.minLength(5), Validators.required]],
+    importance: ['normal'],
+    dueDate: [new Date()],
+    done: [false],
+  });
+
+  constructor(private taskService: TaskService, private router: Router, private fb: FormBuilder) {
+  }
+  ngOnInit(): void {
+    this.taskForm.valueChanges.subscribe(formValue => {
+      if (formValue.title && formValue.title?.length < 5) {
+        this.taskForm.controls.importance.setErrors({ importanceImportant: true })
+      } else {
+        this.taskForm.controls.importance.setErrors(null);
+      }
+    })
+
 
   }
-  submit(task: Task) {
-    console.log(`submitted`, task);
-    this.taskService.createTask(task)
-      .subscribe({
-        next: task => {
-          console.log(`am creat`, task);
-          this.router.navigate(['/tasks']);
-        },
-        error: (err) => alert('NU ESTE BACKEND!')
-      });
+  submit() {
+    console.log(`submitted`, this.taskForm.value);
+    this.taskForm.disable();
+    setTimeout(() => {
+      this.taskService.createTask(<Task>this.taskForm.value)
+        .subscribe({
+          next: task => {
+            console.log(`am creat`, task);
+            this.router.navigate(['/tasks']);
+            this.taskForm.enable();
+          },
+          error: (err) => alert('NU ESTE BACKEND!')
+        });
+    }, 5000);
+
   }
 }
