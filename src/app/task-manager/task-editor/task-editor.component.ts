a3797cb9e27bf63aae01900682cb45b3306ef4f8
Ilie Ciotir <ilie.ciotir@sap.com>
2023-12-15 11:10:57 +0200

    add entire form and css

--- src/app/task-manager/task-editor/task-editor.component.ts
+++ src/app/task-manager/task-editor/task-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Task } from '../task-service/task.service';
 
 @Component({
   selector: 'app-task-editor',
@@ -11,4 +12,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class TaskEditorComponent {
 
+  submit(task: Task) {
+    console.log(`submitted`, task);
+  }
 }
